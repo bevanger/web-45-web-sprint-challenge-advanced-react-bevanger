a05bf135eb719e1b1deda9acabb0ca6fd045f0fb
bevanger <baileyevanger@gmail.com>
2021-08-06 10:46:26 -0600

    wrote first test to check for header

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+});
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+    const checkoutHeader = screen.getByText(/checkout form/i);
+    expect(checkoutHeader).toHaveTextContent(/checkout form/i);
+});
 
-test("form shows success message on submit with form details", () => {});
+
+test("form shows success message on submit with form details", async () => {
+    
+
+});
